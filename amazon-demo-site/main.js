72b16260c60b1e646e809708157d5be7424ee72c
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2022-09-18 16:07:50 +0200

    Fix variable name and indentation

--- amazon-demo-site/main.js
+++ amazon-demo-site/main.js
@@ -1,18 +1,19 @@
 const navMenu = document.getElementById('nav-menu')
-toggleMenu = document.getElementById('toggle-menu')
-closeMenu = document.getElementById('close-menu')
+const toggleMenu = document.getElementById('toggle-menu')
+const closeMenu = document.getElementById('close-menu')
 
 toggleMenu.addEventListener('click', () => {
     navMenu.classList.toggle('show_menu')
 })
+
 closeMenu.addEventListener('click', () => {
     navMenu.classList.remove('show_menu')
 })
 // link-1
 
 const navMenus = document.getElementById('nav-menu-1')
-toggleMenus = document.getElementById('toggle-menu-1')
-closeMenus = document.getElementById('close-menu-1')
+const toggleMenus = document.getElementById('toggle-menu-1')
+const closeMenus = document.getElementById('close-menu-1')
 
 toggleMenus.addEventListener('click', () => {
     navMenus.classList.toggle('show_menu_1')
@@ -22,12 +23,12 @@ closeMenus.addEventListener('click', () => {
 })
 
 const navMenua = document.getElementById('nav-menu-2')
-      toggleMenua = document.getElementById('toggle-menu-2')
-      closeMenua = document.getElementById('close-menu-2')
+const toggleMenua = document.getElementById('toggle-menu-2')
+const closeMenua = document.getElementById('close-menu-2')
 
-toggleMenus.addEventListener('click', () => {
+toggleMenua.addEventListener('click', () => {
     navMenua.classList.toggle('show_menu_2')
 })
-closeMenus.addEventListener('click', () => {
+closeMenua.addEventListener('click', () => {
     navMenua.classList.remove('show_menu_2')
 })
