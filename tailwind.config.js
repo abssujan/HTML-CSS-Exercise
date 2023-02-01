b7400241a12bc284dded576a029ca8deb96c8393
Abdus Sabur Sujan <abdussabursujan@gmail.com>
2023-02-01 10:48:36 -0500

    Sangskritayan home page complete version

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,26 @@ module.exports = {
       colors: {
         'meetup-blue': '#00455D',
         'meetup-purple': '#1F24CC',
-      }
+      },
+      inset: {
+        '100': '26rem',
+        '101': '32rem',
+        '102': '42rem',
+      },
+      width: {
+        '97': '50rem',
+        '98': '60rem'
+      },
+      screens: {
+        xll: '1440px',
+      },
+
+    },
+  },
+  variants: {
+    extend: {
+      display: ['group-hover'],
+      visibility: ['group-hover']
     },
   },
   plugins: [],
